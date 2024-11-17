c20ddc573db84ae734a63d8e4708b3905aea1909
Matthew Dunfee <matthewgoldshire@hotmail.com>
2024-11-17 12:48:55 -0600

    django project setup with postgreSQL backend. env gitignore and configureation of the settings file are complete.

---
+++ Prismatica/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Prismatica project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Prismatica.settings')
+
+application = get_wsgi_application()
