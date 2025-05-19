5b70aded262d6bc9f341568899c52a8352c8a9f9
m1dunfee <m1dunfee@UbuntuJellyVM.myguest.virtualbox.org>
2025-05-19 16:00:14 -0500

    replaced the frontend vite with tailwind and postcss with a vite rrd-react app using bootstrap and reactstrap.

---
+++ frontend/app/components/testComp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+function TestComp() {
+  return (
+    <div className="container mt-5">
+      <h1>Hello from Prismatica</h1>
+      <Button color="primary">Click Me</Button>
+    </div>
+  );
+}
+
+export default TestComp;
